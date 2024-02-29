282cfe56fbc77e2d410a450abec0314969a67276
BL4Z3 <lahiru.r@hyperbyte.io>
2024-02-29 10:58:45 +0530

    added edit_link property

--- app/repos/product.js
+++ app/repos/product.js
@@ -2,12 +2,13 @@ const { faker } = require('@faker-js/faker');
 const { nanoid } = require('nanoid');
 
 class Product {
-  constructor(id, name, price, description) {
+  constructor(id, name, price, description, edit_link) {
     this.id = id ?? nanoid(8);
     this.name = name ?? faker.commerce.productName();
     this.price =
       price ?? faker.commerce.price({ min: 1000, max: 10000, dec: 2 });
     this.description = description ?? faker.commerce.productDescription();
+    this.edit_link = edit_link ?? faker.internet.url({ protocol: 'https' });
   }
 
   toString() {
